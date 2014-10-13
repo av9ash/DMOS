a6d3c4fb510df0e58e68eee48d3b39b7293694c9
NiteshKedia <nitesh.kedia5@gmail.com>
2014-10-12 18:37:39 -0700

    print removed

--- Project1/TCB.h
+++ Project1/TCB.h
@@ -13,15 +13,15 @@ typedef struct TCB TCB_t;
 void init_TCB (TCB_t *tcb, void *function, void *stackP, int stack_size)
 {
 
-	printf("Inside Init TCB...\n");
-	printf("1\n");
+	//printf("Inside Init TCB...\n");
+	//printf("1\n");
 	memset(tcb,'\0',sizeof(TCB_t));
-	printf("2\n");
+	//printf("2\n");
 	getcontext(&tcb->context);
 	tcb->context.uc_stack.ss_sp = stackP;
 	tcb->context.uc_stack.ss_size = (size_t) stack_size;
-	printf("4\n");
+	//printf("4\n");
 	makecontext(&tcb->context, function, 0);
-	printf("5\n");
+	//printf("5\n");
 
 }
