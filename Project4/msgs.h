5aa4b21f25e05aa93c0a4395d2f1a66a2f966872
Avinash <avinash2k9@yahoo.com.au>
2019-11-04 21:16:49 -0700

    Update msgs.h
    Added producer and consumer semaphore logic

--- Project4/msgs.h
+++ Project4/msgs.h
@@ -1,51 +1,56 @@
 #include "sem.h"
 
-typedef struct port 
+typedef struct msg
 {
-	char **msgs;
-	int in, out;
-	semaphore_t mutex, full, empty;
+    int data[10];
+}MSG;
+
+typedef struct port
+{
+    int count_unread, count_read;
+    MSG msgs[10];
+    semaphore_t mutex,Full,Empty;
 }PORT;
 
-PORT p[100];
+//Declare a set (array of ports). The ports are numbered 0 to 99
+PORT ports[100];
 
-
-void init_port() 
+void init_port()
 {
-	printf("\n\t\t\t\t\t\tInitialize Ports\n");
-	printf("\t\t\t\t\t\t*******************\n");
-	int i=0;
-	for(i=0;i<100;i++)
-	{
-		p[i].msgs=(char **) malloc(10*sizeof(char *));
-		int j=0;
-		for(j=0;j<10;j++)
-		    p[i].msgs[j]=(char *) malloc(20*sizeof(char));
-		init_sem(&p[i].full,0);
-		init_sem(&p[i].empty,10);
-		init_sem(&p[i].mutex,1);
-	}
-}
-	
+    for(int i=0;i<100;i++)
+    {
+        ports[i].count_unread = 0;
+        ports[i].count_read = 0;
 
-void send(PORT *p, char *msg,int portnum)
-{
-//    P(&p->empty);
-    P(&p->mutex);
-	strcpy(p->msgs[p->in],msg); 
-  //	printf("Message sent to port %d at index: %d\n",portnum,p->in);
-	p->in = ((p->in)+1)%10;
-    V(&p->mutex);
- //   V(&p->full);
+        ports[i].Full = create_sem(0);
+        ports[i].mutex = create_sem(1);
+        ports[i].Empty = create_sem(10);
+    }
 }
 
-void receive(PORT *p, char *msg,int portnum)
+void send(PORT *port, MSG *msg)
 {
-  // P(&p->full);
-    P(&p->mutex);
-	strcpy(msg,p->msgs[p->out]);	
-  	//printf("Message read from port %d at index: %d\n",portnum,p->out);
-  	p->out = ((p->out)+1)%10;
-    V(&p->mutex);
-   // V(&p->empty);
+    P(&ports->Empty);
+    P(&ports->mutex);
+
+    int msg_num = port->count_unread;
+    port->msgs[msg_num] = *msg;
+    port->count_unread = (msg_num+1)%10;
+
+    V(&ports->mutex);
+    V(&ports->Full);
+}
+
+MSG receive(PORT *port)
+{
+    P(&port->Full);
+    P(&ports->mutex);
+
+    int msg_num = port->count_read;
+    MSG msg = port->msgs[msg_num];
+    port->count_read = (msg_num+1)%10;
+
+    V(&ports->mutex);
+    V(&ports->Empty);
+    return msg;
 }
